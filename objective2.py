25315911b20d5acdff386c86ae9138d2de9526cb
gokulrameshd <gorad364@gmail.com>
2021-07-31 10:33:00 +0530

    second

--- objective2.py
+++ objective2.py
@@ -2,42 +2,44 @@ from math import *
 from objective1 import *
 
 
-class polygons_sequence:
-    """
-    This class returns the returns an objects -> iterable & iterator.
-    It also have properties such maximum efficient polygon.
-    """
-    def __init__(self,n,r):
-        self.n = n
-        if n < 3:
-            raise ValueError("n must be greater then 3")
-        self.r = r
-        self._polygons = list([polygon((i), r )  for (i) in range(3,self.n+1)])
-        self.maximum_efficient_polygon = self.max_efficiency_polygons()
-#         self.iter_polygon = iter(self._polygons)
-        self.index = 0
-    def __repr__(self):
-        return f'Polygon sequence of {len(self._polygons)} polygons'
-
+class Polygons: 
+    "Iterable Class"
+    def __init__(self, m, R):
+        self._m = m-2
+        self._R = R
     def __len__(self):
-        return len(self._polygons)
+        return self._m 
 
-    def __getitem__(self,s):
-        return self._polygons[s] #next(self._polygons[s])
+    def __getitem__(self, i):
+        print(i)
+        if i+3 >= self._m:
+            raise StopIteration
+        else:
+#             [Polygon(i, R) for i in range(3, m+1)]
+            print(self._m,Polygon(i+3 ,self._R))
+            return Polygon(i+3 ,self._R)
+            
+class PolygonsIterator:
+    "Iterator class"
+    def __init__(self, m, R):
+        self._polygons = Polygons(m ,R)
+        print(len(self._polygons))
+        self._i = 0
+        
     def __iter__(self):
         return self
+    
     def __next__(self):
-#         return next(self.iter_polygon)
-        try:
-            result = self._polygons[self.index]
-        except IndexError:
+        if self._i >= len(self._polygons):
             raise StopIteration
-        self.index += 1
-        return result
-    def max_efficiency_polygons(self):
-        seq = sorted(self._polygons,
-                    key = lambda p:p.area/p.perimeter,
-                    reverse = True)
-        # print(seq)
-        return seq[0]
+        else:
+            result = self._polygons[self._i]
+            self._i += 1
+            return result
+    @property
+    def max_efficiency_polygon(self):
+        sorted_polygons = sorted(self._polygons, 
+                                 key=lambda p: p.area/p.perimeter,
+                                reverse=True)
+        return sorted_polygons[0]
     
--- test_session12.py
+++ test_session12.py
@@ -7,55 +7,55 @@ import pytest
 
 
 def test_1():
-    polygon1 = polygon(n =5 , r =10)
-    assert polygon1.edges == 5 , "Wrong number of edges!!"
+    polygon1 = Polygon(n =5 , R =10)
+    assert polygon1.count_edges == 5 , "Wrong number of edges!!"
 
 def test_2():
-    polygon1 = polygon(n =5 , r =10)
-    assert polygon1.vertices == 5 , "Wrong number of vertices!!"
+    polygon1 = Polygon(n =5 , R =10)
+    assert polygon1.count_vertices == 5 , "Wrong number of vertices!!"
 
 def test_3():
-    polygon1 = polygon(n =5 , r =10)
-    assert polygon1.edge_length == 11.755705045849464 , "edge_length is calculation is wrong!!"
+    polygon1 = Polygon(n =5 , R =10)
+    assert polygon1.side_length == 11.755705045849464 , "edge_length is calculation is wrong!!"
 
 def test_4():
-    polygon1 = polygon(n =5 , r =10)
+    polygon1 = Polygon(n =5 , R =10)
     assert polygon1.interior_angle == 108 , "interior_angle is calculation is wrong!!"
 
 def test_5():
-    polygon1 = polygon(n =5 , r =10)
+    polygon1 = Polygon(n =5 , R =10)
     assert polygon1.apothem == 8.090169943749475 , "apothem is calculation is wrong!!"
 
 def test_6():
-    polygon1 = polygon(n =5 , r =10)
+    polygon1 = Polygon(n =5 , R =10)
     assert polygon1.perimeter ==  58.77852522924732 , "perimeter is calculation is wrong!!"
 
 def test_7():
-    polygon1 = polygon(n =5 , r =10)
+    polygon1 = Polygon(n =5 , R =10)
     assert polygon1.area ==  237.76412907378844 , "area is calculation is wrong!!"
 
 def test_8():
-    polygon1 = polygon(n =5 , r =10)
-    assert polygon1.__repr__() == "No of sides :5 , Radius : 10" , "__repr__ is not working !!"
+    polygon1 = Polygon(n =5 , R =10)
+    assert polygon1.__repr__() == "Polygon(n=5, R=10)" , "__repr__ is not working !!"
 
 def test_9():
-    polygon1 = polygon(n =5 , r =10) # the initializer takes in  
-    polygon2 = polygon(n =5 , r =10)
+    polygon1 = Polygon(n =5 , R =10) # the initializer takes in  
+    polygon2 = Polygon(n =5 , R =10)
     assert polygon1 == polygon2 , " __eq__ is not working !!"
 
 def test_10():
-    polygon1 = polygon(n =5 , r =10) # the initializer takes in  
-    polygon2 = polygon(n =4 , r =10)
+    polygon1 = Polygon(n =5 , R =10) # the initializer takes in  
+    polygon2 = Polygon(n =4 , R =10)
     assert polygon1 > polygon2 , " __gt__ is not working !!"
 
 def test_11():
-    poly_seq = polygons_sequence(n = 25 , r =100)
+    poly_seq = Polygons(m = 25 , R =100)
     assert len(poly_seq) == 23 , " __len__ is not working !!"
 
-def test_12():
-    poly_seq = polygons_sequence(n = 25 , r =100)
-    assert str(next(poly_seq)) == "No of sides :3 , Radius : 100" , "__next__ is not working!!"
+# def test_12():
+#     poly_seq = Polygons(n = 25 , R =100)
+#     assert str(next(poly_seq)) == "No of sides :3 , Radius : 100" , "__next__ is not working!!"
 
-def test_13():
-    poly_seq = polygons_sequence(n = 25 , r =100)
-    poly_seq.__iter__() == "Polygon sequence of 23 polygons" , "__iter__ is not working!!"
+# def test_13():
+#     poly_seq = Polygons(n = 25 , R =100)
+#     poly_seq.__iter__() == "Polygon sequence of 23 polygons" , "__iter__ is not working!!"
